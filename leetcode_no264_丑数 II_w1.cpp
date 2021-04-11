e47e863469c84ee6203ffb53c7d87cbf473327b8
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-04-11 18:59:38 +0800

    Add files via upload

---
+++ leetcode_no264_丑数 II_w1.cpp
@@ -0,0 +1,93 @@
+﻿/* ------------------------------------------------------------------|
+给你一个整数 n ，请你找出并返回第 n 个 丑数 。
+
+丑数 就是只包含质因数 2、3 和/或 5 的正整数。
+
+ 
+
+示例 1：
+
+输入：n = 10
+输出：12
+解释：[1, 2, 3, 4, 5, 6, 8, 9, 10, 12] 是由前 10 个丑数组成的序列。
+示例 2：
+
+输入：n = 1
+输出：1
+解释：1 通常被视为丑数。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/ugly-number-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	双指针法
+*
+*	执行用时：0 ms, 在所有 C++ 提交中击败了100.00%的用户
+*	内存消耗：8.1 MB, 在所有 C++ 提交中击败了81.94%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+int nthUglyNumber(int n) {
+    vector<int> dp(n + 1);
+    dp[1] = 1;
+    int p2 = 1;
+    int p3 = 1;
+    int p5 = 1;
+    for (int i = 2; i <= n; ++i) {
+        int num2 = dp[p2] * 2;
+        int num3 = dp[p3] * 3;
+        int num5 = dp[p5] * 5;
+        dp[i] = min(min(num2, num3), num5);
+        if (dp[i] == num2) {
+            p2++;
+        }
+        if (dp[i] == num3) {
+            p3++;
+        }
+        if (dp[i] == num5) {
+            p5++;
+        }
+    }
+    return dp[n];
+}
